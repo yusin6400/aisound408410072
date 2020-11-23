d9c2b8ace971b0e637916fdd443460bc9d39a583
yusin6400 <64146523+yusin6400@users.noreply.github.com>
2020-11-23 14:59:42 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://yusin6400.github.io/aisound408410072/model.json';
 
 
 function preload() {
